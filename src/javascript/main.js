9a2c646c161965ce096df7d0671b80d241fa5008
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-26 12:20:26 -0300

    add chamada function in the html and adjust parameters in the main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,21 +1,22 @@
-const PassworBox = document.getElementById("password");
+const passwordBox = document.getElementById("password");
 const length = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
-const symbols = "@#$%^&*()_+~|}{[]></-=";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
 
-const allChars = upperCase + lowerCase + number + symbols;
+const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword() {
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCaserCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
-    password += symbols[Math.floor(Math.random() * symbols.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
 
     while(length > password.length) {
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
+    passwordBox.value = password;
 }

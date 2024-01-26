2cf839896e31de662385e60d4678d575a53f7582
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-26 11:47:19 -0300

    create const number and const symbols

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,3 +3,5 @@ const length = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "@#$%^&*()_+~|}{[]></-=";
